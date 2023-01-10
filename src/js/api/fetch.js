cfe966bd20515197c906a35e3bb91c5fa1486a61
a-pinchuk <co.born2code@gmail.com>
2023-01-10 13:32:42 +0200

    adding fetching_methods

--- src/js/api/fetch.js
+++ src/js/api/fetch.js
@@ -1,13 +1,26 @@
 import axios from 'axios';
 import { API_KEY, TREND_URL, SEARCH_URL, ID_URL } from './api-vars';
 
-const baseUrl = 'https://api.themoviedb.org/3/trending/all/day?';
-
-export async function fetchImg(content, page) {
+export async function fetchTrendedFilms() {
   const searchParams = new URLSearchParams({
     api_key: API_KEY,
-    // query: content,
-    // page: page,
   });
-  return axios.get(`${baseUrl}${searchParams}`);
+  return axios.get(`${TREND_URL}${searchParams}`);
+}
+
+export async function fetchSearchedFilms(content, page) {
+  const searchParams = new URLSearchParams({
+    api_key: API_KEY,
+    query: content,
+    page: page,
+  });
+  return axios.get(`${SEARCH_URL}${searchParams}`);
+}
+
+export async function fetchFilmById(id) {
+  const movie_id = id;
+  const searchParams = new URLSearchParams({
+    api_key: API_KEY,
+  });
+  return axios.get(`${ID_URL}${movie_id}?${searchParams}`);
 }
