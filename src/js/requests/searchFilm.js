4d625587aa2cc4b7c1bc30874f2ad94d95c883d8
EvaVozz <voznukeeva@ukr.net>
2023-01-10 22:58:12 +0200

    adding

--- src/js/requests/searchFilm.js
+++ src/js/requests/searchFilm.js
@@ -1,8 +1,15 @@
 
 import { fetchSearchedFilms } from "../api/fetch";
+import { ref } from "../references/ref";
 
 let page = 1;
 let content = '';
 
 
-fetchSearchedFilms();
+async function renderSearchFilms() {
+    const data = await fetchSearchedFilms(content, page);
+    console.log(data);
+}
+
+renderSearchFilms();
+
