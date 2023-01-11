f204404021d4e8e42c4ba510fd97a0ed1cfd16df
EvaVozz <voznukeeva@ukr.net>
2023-01-11 15:40:13 +0200

    adding

--- src/js/references/ref.js
+++ src/js/references/ref.js
@@ -1,7 +1,7 @@
 const ref = {
   form: document.querySelector('.search-form'),
-  input: document.querySelector('input'),
-  button: document.querySelector('button'),
+  input: document.querySelector('.search-form__input'),
+  button: document.querySelector('.search-form__button'),
   gallery: document.querySelector('.gallery'),
   hideButton: document.querySelector('.load__more'),
   loader: document.getElementById('loading'),
--- src/js/requests/searchFilm.js
+++ src/js/requests/searchFilm.js
@@ -1,8 +1,8 @@
 
 import { fetchSearchedFilms } from "../api/fetch";
 import { ref } from "../references/ref";
-
-const defaultPic = 'https://w7.pngwing.com/pngs/116/765/png-transparent-clapperboard-computer-icons-film-movie-poster-angle-text-logo-thumbnail.png'
+ 
+let IMG__URL = `https://image.tmdb.org/t/p/w500/`;
 
 let page = 1;
 
@@ -11,7 +11,7 @@ let page = 1;
 ref.form.addEventListener('submit', clickOnSubmit);
 function clickOnSubmit(e) {
     e.preventDefault();
-    content = ref.input.value;
+    content = ref.input.name.value;
     renderSearchFilms();
     
     
@@ -30,22 +30,66 @@ async function renderSearchFilms() {
    
 }
 
+
 function makeMarkUpSearchFilm(cardValue) {
-    const cardEle = cardValue.map(item => ({
+    const cardEle = cardValue.map(item => {
+        const info = {
         title: item.title,
         backImgPost: defaultImage(item.poster_path),
         filmId: item.IdleDeadline,
         ganerId: item.genre_ids,
-        
-
-    }))
+        }
+        check(info.title)
+    })
     console.log(cardEle);
+
+    
 }
 
+
+// function makeMarkUpSearchFilm(cardValue) {
+//     const cardEle = cardValue.map(item => ({
+        
+//         title: item.title,
+//         backImgPost: defaultImage(item.poster_path),
+//         filmId: item.IdleDeadline,
+//         ganerId: item.genre_ids,
+        
+
+//     }))
+//     console.log(cardEle);
+// }
+
 function defaultImage(poster) {
-    if (poster) {
-        return `https://image.tmdb.org/t/p/w500/${poster}`;
+    if (IMG__URL) {
+        return  IMG__URL = `https://image.tmdb.org/t/p/w500/${poster}`;
     } else {
-        return defaultPic;
+        return 'https://assets-in.bmscdn.com/iedb/movies/images/website/poster/large/enn-nenjai-thottaye-et00021963-24-03-2017-18-55-43.jpg';
     }
+}
+
+function createMarkUp() {
+    const listElemeny =
+     `
+      <ul class="gallery__list">
+    <li class="gallery__link"><a href="" class="gallery__link">
+      <img src="" alt="">
+      <div class="gallery__box">
+        <h2 class="gallery__title">
+          <div class="gallery__info">
+                <p class="gallery__ganer">
+                <p class="gallery__year"></p>
+          </div>
+     
+          </p>
+        </h2>
+      </div>
+    </a></li>
+   </ul>
+    `;
+    ref.gallery.insertAdjacentHTML('beforeend', listElemeny);
+}
+createMarkUp();
+function check(callback) {
+    console.log(callback);
 }
