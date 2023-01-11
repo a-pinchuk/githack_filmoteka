d378e88262867521ce57d17173dcee4f6108abbf
a-pinchuk <co.born2code@gmail.com>
2023-01-11 15:00:18 +0200

    rendering finished

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import './js/api/api-vars';
 import './js/api/fetch';
 import './js/references/ref';
 import './js/authentication/authentication';
+import './js/main';
 
 // Переключатель темы
--- src/js/main.js
+++ src/js/main.js
@@ -1,52 +1,19 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchTrendedFilms } from './api/fetch';
-import { fetchSearchedFilms } from './api/fetch';
-import { fetchFilmById } from './api/fetch';
-import { fetchFilmGenres } from './api/fetch';
-import { renderPopularFilms } from './render/renderHTML';
-import { renderFilms } from './render/renderHTML';
+// import { fetchSearchedFilms } from './api/fetch';
+// import { fetchFilmById } from './api/fetch';
+// import { fetchFilmGenres } from './api/fetch';
+import { renderPopularFilms } from './render/renderPopularFilm';
+// import { renderFilms } from './render/renderHTML';
 import { ref } from './references/ref';
-import { options, pagination } from './pagination';
-import { PAGES } from './api/api-vars';
+// import { options, pagination } from './pagination';
+// import { PAGES } from './api/api-vars';
 
-let textContent = '';
+// let textContent = '';
 
-ref.form.addEventListener('submit', fetchAndRenderFilms);
+// ref.form.addEventListener('submit', fetchAndRenderFilms);
 
 fetchAndRenderPopularFilm();
-fetchAndRenderFilmById(89125);
-fetchFilmByGenres();
-
-async function fetchAndRenderFilms(e) {
-  e.preventDefault();
-  textContent = e.currentTarget.searchQuery.value;
-
-  clearData();
-  if (textContent === '') {
-    clearData();
-    return Notify.warning(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-  }
-  try {
-    const image = await fetchSearchedFilms(textContent, PAGES);
-    const data = image.data.results;
-    const total_pages = image.data.total_pages;
-    console.log('🚀 ~ data in fetchAndRenderFilms', total_pages);
-
-    if (data.length === 0) {
-      return Notify.warning(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    } else {
-      Notify.success(`Hooray! We found ${image.data.totalHits} images.`);
-    }
-    renderFilms(ref, data);
-    pagination.on('afterMove', ({ page }) => console.log(page));
-  } catch (error) {
-    console.log(error);
-  }
-}
 
 async function fetchAndRenderPopularFilm(e) {
   try {
@@ -59,40 +26,73 @@ async function fetchAndRenderPopularFilm(e) {
     console.log(error);
   }
 }
-async function fetchAndRenderFilmById(id) {
-  try {
-    const image = await fetchFilmById(id);
-    const data = image.data;
-    console.log('🚀 ~ data in fetchAndRenderFilmById', data);
+// async function fetchAndRenderFilmById(id) {
+//   try {
+//     const image = await fetchFilmById(id);
+//     const data = image.data;
+//     console.log('🚀 ~ data in fetchAndRenderFilmById', data);
 
-    // renderPopularFilms(ref, data);
-  } catch (error) {
-    console.log(error);
-  }
-}
+//     // renderPopularFilms(ref, data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// fetchAndRenderFilmById(89125);
+// fetchFilmByGenres();
 
-async function fetchFilmByGenres() {
-  try {
-    const image = await fetchFilmGenres();
-    GENRES = image.data;
-  } catch (error) {
-    console.log(error);
-  }
-}
+// async function fetchAndRenderFilms(e) {
+//   e.preventDefault();
+//   textContent = e.currentTarget.searchQuery.value;
 
-var counter = 0;
+//   clearData();
+//   if (textContent === '') {
+//     clearData();
+//     return Notify.warning(
+//       'Sorry, there are no images matching your search query. Please try again.'
+//     );
+//   }
+//   try {
+//     const image = await fetchSearchedFilms(textContent, PAGES);
+//     const data = image.data.results;
+//     const total_pages = image.data.total_pages;
+//     console.log('🚀 ~ data in fetchAndRenderFilms', total_pages);
 
-document.querySelectorAll('input').forEach(function (input) {
-  input.addEventListener('input', function (e) {
-    counter = input.value.length * 4;
-    document.querySelector('.underline').style.width = counter + '%';
+//     if (data.length === 0) {
+//       return Notify.warning(
+//         'Sorry, there are no images matching your search query. Please try again.'
+//       );
+//     } else {
+//       Notify.success(`Hooray! We found ${image.data.totalHits} images.`);
+//     }
+//     renderFilms(ref, data);
+//     pagination.on('afterMove', ({ page }) => console.log(page));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-    if (input.value.length == 0) {
-      document.querySelector('.underline').style.width = '100%';
-    }
-  });
-});
+// async function fetchFilmByGenres() {
+//   try {
+//     const image = await fetchFilmGenres();
+//     GENRES = image.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-function clearData() {
-  ref.gallery.innerHTML = '';
-}
+// var counter = 0;
+
+// document.querySelectorAll('input').forEach(function (input) {
+//   input.addEventListener('input', function (e) {
+//     counter = input.value.length * 4;
+//     document.querySelector('.underline').style.width = counter + '%';
+
+//     if (input.value.length == 0) {
+//       document.querySelector('.underline').style.width = '100%';
+//     }
+//   });
+// });
+
+// function clearData() {
+//   ref.gallery.innerHTML = '';
+// }
