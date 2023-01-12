4e05bc625e4f67f2c6f000798205d83f996d5716
a-pinchuk <co.born2code@gmail.com>
2023-01-12 15:31:07 +0200

    123123

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import './js/api/fetch';
 import './js/references/ref';
 import './js/authentication/authentication';
 import './js/fetchAndRenderPopularFilm';
-import './js/library';
-import './js/modal';
+// import './js/library';
+// import './js/modal';
 import './js/requests/searchFilms';
 import './js/render/renderSearchFilms';
 
--- src/js/render/renderSearchFilms.js
+++ src/js/render/renderSearchFilms.js
@@ -1,19 +1,23 @@
 import allGeners from '../../json/genres.json';
 console.log(allGeners);
 
-export function createMarkUp(ref,data) {
-  const markUp = data.map(item => {
-    	const defaultPicture = 'https://motivatevalmorgan.com/wp-content/uploads/2016/06/default-movie.jpg'
-    // const defaultPicture = defImg;
-		const { title, poster_path, release_date, genre_ids } = item;
-	  const filmGenre = getGeners(allGeners, genre_ids)
-	  let link = `https://image.tmdb.org/t/p/w500null`;
-		let baseImafge = `https://image.tmdb.org/t/p/w500${poster_path}`;
-		const normalizeDate = release_date.slice(0, 4);
-		return `
+export function createMarkUp(ref, data) {
+  const markUp = data
+    .map(item => {
+      const defaultPicture =
+        'https://motivatevalmorgan.com/wp-content/uploads/2016/06/default-movie.jpg';
+      // const defaultPicture = defImg;
+      const { title, poster_path, release_date, genre_ids } = item;
+      const filmGenre = getGeners(allGeners, genre_ids);
+      let link = `https://image.tmdb.org/t/p/w500null`;
+      let baseImafge = `https://image.tmdb.org/t/p/w500${poster_path}`;
+      const normalizeDate = release_date.slice(0, 4);
+      return `
       <li class="photo__card">
           <a href="${baseImafge}">
-            <img src="${baseImafge !== link ? baseImafge : defaultPicture}" alt="" "loading="lazy" class="movie__image"/>
+            <img src="${
+              baseImafge !== link ? baseImafge : defaultPicture
+            }" alt="" "loading="lazy" class="movie__image"/>
           </a>
           <div class="movie__info">
             <h2 class="film__title">${title}</h2>
@@ -25,22 +29,21 @@ export function createMarkUp(ref,data) {
       </li>
     </ul>
   </div>`;
-
-	}).join('');
-	ref.gallery.insertAdjacentHTML('beforeend', markUp);
+    })
+    .join('');
+  ref.galleryList.insertAdjacentHTML('beforeend', markUp);
 }
-
 
 function getGeners(allGenres, idGenres) {
   let newArray = [];
-     allGenres.map(el => {
+  allGenres.map(el => {
     if (idGenres.includes(el.id)) {
       newArray.push(el.name);
     }
-     });
+  });
   if (newArray.length > 2) {
-	  newArray = newArray.slice(0, 2).join(', '); 
-	  return newArray;
+    newArray = newArray.slice(0, 2).join(', ');
+    return newArray;
   }
   return newArray.join(', ') + ' other';
 }
