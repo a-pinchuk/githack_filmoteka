440581ca0404efa4c242608fe784d01b39743b1b
nadikkk <109106413+nadikkk@users.noreply.github.com>
2023-01-12 22:09:15 +0200

    add-indexLibrary

---
+++ src/indexLibrary.js
@@ -0,0 +1,20 @@
+// Стили
+
+// Скрипты
+import './js/api/api-vars';
+import './js/api/fetch';
+import './js/references/ref';
+
+// import './js/fetchAndRenderPopularFilm';
+import './js/render/renderFilmByIdForLibrary';
+import './js/render/modal-students';
+ import './js/library';
+import './js/modal';
+
+import './js/requests/searchFilms';
+import './js/render/renderSearchFilms';
+
+// Переключатель темы
+
+import './js/switcher';
+import './js/authentication/authentication';
