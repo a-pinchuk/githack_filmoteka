7c98810fe160adbc0eba3caa9d6a7ed3684aff1c
TarasTikan <tikantaras1@gmail.com>
2023-01-13 21:33:31 +0200

    add daad

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { log } from 'console';
 import { API_KEY, TREND_URL, SEARCH_URL, ID_URL } from './api/api-vars';
-import { fetchFilmById, fetchTrendedFilms } from './api/fetch';
+import { fetchFilmById, fetchTrendedFilms, fetchFilmTrailer } from './api/fetch';
 import { ref } from './references/ref';
 import { loaderHide } from '../js/fetchAndRenderPopularFilm';
 
@@ -32,6 +32,24 @@ async function openModal(item) {
   renderBackdrop(response);
   ref.modalWrap.insertAdjacentHTML('afterBegin', renderMarkupModal(response));
   loaderHide();
+  // ====================TREILER=========================================
+  const btnTreil = document.querySelector('.modal-btn-trailer')
+const div = document.querySelector('.modal-wrap-img-btn')
+const wrapIMG = document.querySelector('.modal-img-wrap')
+btnTreil.addEventListener('click', onClickWatch)
+
+async function onClickWatch () {
+  const li = item.target.closest('.photo__card');
+  const id = li.getAttribute('id');
+  const response = await fetchFilmTrailer(id).then(r => {
+    return r.data;
+  })
+  const officialTrail = response.results.length -1
+  wrapIMG.remove();
+  btnTreil.style.display = 'none'
+  ref.modalWrap.insertAdjacentHTML('afterBegin', renderTrail(response.results[officialTrail]));
+}
+  // ====================TREILER========================================
 }
 
 function renderBackdrop(film) {
@@ -107,3 +125,17 @@ function closeModalbyClick(e) {
     document.removeEventListener('keydown', closeModal);
   }
 }
+
+function renderTrail ({key}) {
+  return `<iframe
+  width="375"
+    height="478"
+    src="https://www.youtube.com/embed/${key}"
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen
+    waitUntil()
+    class='modal-image'
+  ></iframe>`
+  }
