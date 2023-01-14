a3aacc339f624efee47c81bd170de8b9d0c5d34b
VozzRoman <looper0500@ukr.net>
2023-01-14 21:55:09 +0200

    adding

--- src/js/requests/searchFilms.js
+++ src/js/requests/searchFilms.js
@@ -5,7 +5,7 @@ import { Notify } from 'notiflix';
 import { PAGE } from '../api/api-vars';
 import { renderPopularFilms } from '../render/renderPopularFilm';
 import { loaderHide } from '../fetchAndRenderPopularFilm';
-import Pagination from 'tui-pagination';
+// import Pagination from 'tui-pagination';
 
 let searchQuery = '';
 PAGE = 2;
@@ -14,7 +14,9 @@ ref.form.addEventListener('submit', onCLickSubmit);
 function onCLickSubmit(e) {
   e.preventDefault();
 	searchQuery = ref.input.value.trim();
+	ref.loader.style.display = 'flex';
 	if (searchQuery === '') {
+		loaderHide();
    	ref.alertMessage.textContent = 'Searching starts after providing data to search';
 	  Notify.warning('Searching starts after providing data to search.');
 	  return;
@@ -35,18 +37,17 @@ async function renderSearchFilms() {
     
     const promis = await fetchSearchedFilms(searchQuery, PAGE);
     const data = promis.data.results;
-    const paginationData = promis.data;
+   //  const paginationData = promis.data;
     
     if (data.length === 0) {
+		loaderHide();
       ref.alertMessage.textContent = 'Search result not successful. Enter the correct movie name and '
       return Notify.warning('Search result not successful. Enter the correct movie name and ');
     }
     ref.alertMessage.textContent = '';
     ref.loader.style.display = 'flex';
     clearGallery();
-    
-	 
-	  paginationPage(paginationData);
+	// paginationPage(paginationData);
 	   createMarkUp(ref, data);
     loaderHide();
   } catch (error) {
@@ -65,43 +66,45 @@ function clearGallery() {
 // TUI pagination-------
 // const data = 440;
 // paginationPage(data);
-function paginationPage(dataPag) {
-	const container = document.getElementById('pagination');
-	console.log(dataPag.page)
-  const itemsPerPage = 20;
-  const options = {
-    totalItems: dataPag.total_results,
-    itemsPerPage,
-    visiblePages: 10,
-	  centerAlign: false,
-	        page: '<a href="#" class="tui-page-btn">{{page}}p</a>',
-            currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}p</strong>',
-            moveButton:
-                '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
-                    '<span class="tui-ico-{{type}}">{{type}}</span>' +
-                '</a>',
-            disabledMoveButton:
-                '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
-                    '<span class="tui-ico-{{type}}">{{type}}</span>' +
-                '</span>',
-            moreButton:
-                '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
-                    '<span class="tui-ico-ellip">...</span>' +
-                '</a>'
-  };
-	console.log(options.totalItems);
+// function paginationPage(dataPag) {
+// 	const container = document.getElementById('pagination');
+// 	console.log(dataPag.page)
+//   const itemsPerPage = 20;
+//   const options = {
+//     totalItems: dataPag.total_results,
+//     itemsPerPage,
+//     visiblePages: 10,
+// 	  centerAlign: false,
+// 	        page: '<a href="#" class="tui-page-btn">{{page}}p</a>',
+//             currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}p</strong>',
+//             moveButton:
+//                 '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
+//                     '<span class="tui-ico-{{type}}">{{type}}</span>' +
+//                 '</a>',
+//             disabledMoveButton:
+//                 '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
+//                     '<span class="tui-ico-{{type}}">{{type}}</span>' +
+//                 '</span>',
+//             moreButton:
+//                 '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
+//                     '<span class="tui-ico-ellip">...</span>' +
+//                 '</a>'
+//   };
+// 	console.log(options.totalItems);
 	
-  const pagination = new Pagination(container, options);
+//   const pagination = new Pagination(container, options);
 
 
-	console.log(pagination);
-	pagination.on('beforeMove', event => {
-		dataPag.page = event.page;
+// 	console.log(pagination);
+// 	pagination.on('beforeMove', event => {
+// 		dataPag.page = event.page;
+// 		clearGallery();
+// 		// createMarkUp(ref, data);
+// 		const promis = fetchSearchedFilms(searchQuery, PAGE);
+//     	console.log(promis);
 		
+		
+// 	})
 	
-		
-		
-	})
-	
-	pagination.movePageTo(0);
-}
+// 	pagination.movePageTo(1);
+// }
