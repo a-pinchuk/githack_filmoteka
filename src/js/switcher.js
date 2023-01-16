7e1f9c70df114a6e667f448f3fff9a5a663ea7cf
ritamirgorodska <ritamirgorodska.1604@gmail.com>
2023-01-16 12:14:54 +0300

    add dark theme on footer

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -18,5 +18,9 @@ function onChange(e) {
     ? document.querySelector('body').classList.add('dark-theme')
     : document.querySelector('body').classList.remove('dark-theme');
 
+  e.target.checked
+    ? document.querySelector('footer').classList.add('dark-theme__footer')
+    : document.querySelector('footer').classList.remove('dark-theme__footer');
+
   localStorage.setItem('darkmode', JSON.stringify(e.target.checked));
 }
